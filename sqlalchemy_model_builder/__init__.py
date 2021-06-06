7b50a7dfc985717828268260e73b83edf5a30307
Amin Alaee <mohammadamin.alaee@gmail.com>
2021-06-06 11:23:04 +0430

    uplift-package-version (#3)
    uplift-package-version

--- sqlalchemy_model_builder/__init__.py
+++ sqlalchemy_model_builder/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 from sqlalchemy_model_builder.exceptions import ModelBuilderException
 from sqlalchemy_model_builder.model_builder import ModelBuilder
