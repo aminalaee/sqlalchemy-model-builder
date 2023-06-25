74e76f1ea12cb6c6c712607b122b2ff26c2b137b
Amin Alaee <mohammadamin.alaee@gmail.com>
2023-06-25 09:58:19 +0200

    Version 0.2.0 (#19)

--- sqlalchemy_model_builder/__init__.py
+++ sqlalchemy_model_builder/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from sqlalchemy_model_builder.exceptions import ModelBuilderException
 from sqlalchemy_model_builder.model_builder import ModelBuilder
